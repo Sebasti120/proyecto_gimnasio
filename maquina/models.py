d28a1fed4b58786c97d3411e84792b3b5553211d
sebasti120 <sebas.iafusa3@gmail.com>
2026-09-03 10:17:28 -0500

    se elimina parte de codigo que no va en maquina

--- maquina/models.py
+++ maquina/models.py
@@ -9,11 +9,3 @@ class Mantenimiento(models.Model):
 
     def __str__(self):
         return f"Mantenimiento de {self.equipo.nombre} - {self.estado}"
-    
-class CondicionMedica(models.Model):
-    nombre = models.CharField(max_length=100)  # Ej: "Asma", "Lesión de rodilla", "Hipertensión"
-    descripcion = models.TextField(blank=True, null=True) # Detalles o recomendaciones especiales
-    requiere_autorizacion = models.BooleanField(default=False) # Si necesita aval médico para entrenar
-
-    def __str__(self):
-        return self.nombre
