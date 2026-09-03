ef47793e140c4dd1265bdb7d516f824369d75133
Harold Orjuela <orjuelaandres108@gmail.com>
2026-09-03 10:48:52 -0500

    Correccion

--- maquina/models.py
+++ maquina/models.py
@@ -21,27 +21,3 @@ class Equipo(models.Model):
 
     def __str__(self):
         return f"{self.marca} - {self.categoria} ({self.numero_serie})"
-
-#-----------Definicion de la enumeracion de estados----------------#
-
-class Sesion(models.Model):
-
-    class EstadoSesion(models.TextChoices):
-        PROGRAMADA = 'PROGRAMADA', 'Programada'
-        EN_PROCESO = 'EN_PROCESO', 'En Proceso'
-        COMPLETADA = 'COMPLETADA', 'Completada'
-        CANCELADA = 'CANCELADA', 'Cancelada'
-
-    # Campos del modelo
-    nombre = models.CharField(max_length=100)
-    fecha = models.DateField()
-    
-    # Campo que utiliza la enumeración
-    estado = models.CharField(
-        max_length=20,
-        choices=EstadoSesion.choices,
-        default=EstadoSesion.PROGRAMADA,
-    )
-
-    def __str__(self):
-        return f"Sesion {self.nombre} - {self.get_estado_display()}"
