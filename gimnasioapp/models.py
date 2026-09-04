e74a9afbaf1d49007663da590231a0b6118c34e2
sebasti120 <sebas.iafusa3@gmail.com>
2026-09-04 11:21:11 -0500

    cambio de la pagina home y clase ejercicio

--- gimnasioapp/models.py
+++ gimnasioapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Ejercicio(models.Model):
+    nombre = models.CharField(max_length=120)
+    grupo_muscular = models.CharField(max_length=80, blank=True)
+    tipo = models.CharField(max_length=50, blank=True)
+    descripcion_tecnica = models.TextField(blank=True)
+    contraindicaciones = models.TextField(blank=True)
+    imagen = models.ImageField(upload_to='ejercicios/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
